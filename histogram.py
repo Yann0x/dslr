591113a1a47cca423f783b6b6a8a96180bb73eaa
1Batmain <batduval@gmail.com>
2026-03-30 19:15:57 +0200

    histoDone

--- histogram.py
+++ histogram.py
@@ -1,30 +1,60 @@
-import matplotlib
-
-matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_histogram(data: pd.DataFrame, column: str):
-
-    field = data[column]
-    field.dropna()
-    if not pd.api.types.is_numeric_dtype(field) or field.empty:
-        print(f"Column {column} is non numeric, skipping it...")
+def plot_histogram(data: pd.DataFrame, field: str):
+    column = data[field]
+    column.dropna()
+    if not pd.api.types.is_numeric_dtype(column) or column.empty or field == "Index":
+        print(f"Column {field} is non relevant, skipping it...")
         return
+    house_colors = {
+        "Ravenclaw": "royalblue",
+        "Slytherin": "seagreen",
+        "Gryffindor": "firebrick",
+        "Hufflepuff": "gold",
+    }
+    for house, color in house_colors.items():
+        house_values = data.loc[data["Hogwarts House"] == house, field].dropna()
+        if house_values.empty:
+            continue
+        plt.hist(house_values, bins=30, alpha=0.45, color=color, label=house)
 
-    plt.hist(field)
+    plt.title(field)
+    plt.legend()
     plt.show()
+
+
+# def plot_histogram(scores: dict):
+
+#     min = max = scores[next(iter(scores))]
+#     for value in scores.values():
+#         if value > max:
+#             max = value
+#         if value < min:
+#             min = value
+
+#     plt.bar(*zip(*scores.items()))
+#     # plt.ylim(min, max)
+#     # plt.tight_layout()
+#     plt.show()
+
+
+def get_scores(data: pd.DataFrame, field: str, house: str):
+    values = data.loc[data["Hogwarts House"] == house, field].dropna()
+    res = values.sum()
+    print(res)
+    return res
 
 
 def main():
     data = pd.read_csv("./datasets/dataset_train.csv")
-    # Source - https://stackoverflow.com/a/79037727
-    # Posted by WhaSukGO
-    # Retrieved 2026-03-30, License - CC BY-SA 4.0
 
-    for column in data.columns:
-        plot_histogram(data, column)
+    for field in data.columns:
+        plot_histogram(
+            data,
+            field,
+        )
 
 
 if __name__ == "__main__":
