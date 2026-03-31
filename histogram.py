a1a626fcac139933685964b77036a06704c5c125
1Batmain <batduval@gmail.com>
2026-03-31 12:34:13 +0200

    add diference indice to histogram.py

--- histogram.py
+++ histogram.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
 def plot_histogram(data: pd.DataFrame, field: str):
     column = data[field]
     column.dropna()
-    if not pd.api.types.is_numeric_dtype(column) or column.empty or field == "Index":
-        print(f"Column {field} is non relevant, skipping it...")
+    if column.empty:
         return
     house_colors = {
         "Ravenclaw": "royalblue",
@@ -14,47 +14,60 @@ def plot_histogram(data: pd.DataFrame, field: str):
         "Gryffindor": "firebrick",
         "Hufflepuff": "gold",
     }
+    bins = 30
+    hist = []
     for house, color in house_colors.items():
         house_values = data.loc[data["Hogwarts House"] == house, field].dropna()
         if house_values.empty:
             continue
-        plt.hist(house_values, bins=30, alpha=0.45, color=color, label=house)
+        plt.hist(house_values, bins=bins, alpha=0.45, color=color, label=house)
+        hist.append(np.histogram(house_values, bins=bins)[0])
 
-    plt.title(field)
+    trsp_hist = list(zip(*hist))
+    variabilite_indices = []
+
+    for bin in range(len(trsp_hist)):
+        bin_values = [row for row in trsp_hist[bin]]
+        variabilite_indices.append(np.var(bin_values))
+
+    variabilite_indice = sum(variabilite_indices) / bins
+    if not hasattr(plot_histogram, "static_var"):
+        plot_histogram.static_var = (variabilite_indice, field)
+    if variabilite_indice < plot_histogram.static_var[0]:
+        plot_histogram.static_var = (variabilite_indice, field)
+
+    plt.title(f"{field} ({variabilite_indice:.2f})")
     plt.legend()
     plt.show()
-
-
-# def plot_histogram(scores: dict):
-
-#     min = max = scores[next(iter(scores))]
-#     for value in scores.values():
-#         if value > max:
-#             max = value
-#         if value < min:
-#             min = value
-
-#     plt.bar(*zip(*scores.items()))
-#     # plt.ylim(min, max)
-#     # plt.tight_layout()
-#     plt.show()
+    return plot_histogram.static_var
 
 
 def get_scores(data: pd.DataFrame, field: str, house: str):
     values = data.loc[data["Hogwarts House"] == house, field].dropna()
     res = values.sum()
-    print(res)
     return res
 
 
 def main():
     data = pd.read_csv("./datasets/dataset_train.csv")
 
+    best = None
     for field in data.columns:
-        plot_histogram(
+        if (
+            not pd.api.types.is_numeric_dtype(data[field])
+            or data[field].empty
+            or field == "Index"
+        ):
+            continue
+        best = plot_histogram(
             data,
             field,
         )
+        if best is not None:
+            print(f"Current best : {best[1]} with score {best[0]:.2f}")
+    print(
+        f"The course that has an homogoneous distribution across houses is {best[1] if best else 'unknown'}"
+    )
 
 
 if __name__ == "__main__":
