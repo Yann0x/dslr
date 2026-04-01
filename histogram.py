060cbafca81a4b0e4669c2b7a48abe9017a40380
1Batmain <batduval@gmail.com>
2026-04-01 14:00:55 +0200

    dataset as args

--- histogram.py
+++ histogram.py
@@ -1,3 +1,5 @@
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -47,7 +49,18 @@ def get_scores(data: pd.DataFrame, field: str, house: str):
 
 
 def main():
-    data = pd.read_csv("./datasets/dataset_train.csv")
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} <dataset.csv>")
+        sys.exit(1)
+
+    try:
+        data = pd.read_csv(sys.argv[1])
+    except FileNotFoundError:
+        print(f"Error: file '{sys.argv[1]}' not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        sys.exit(1)
 
     # build the fields list:
     fields = []
